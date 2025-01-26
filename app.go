737a928f33e63e95471ec56905cf1d9558212e14
kwintti <sergei.borovskii@gmail.com>
2025-01-26 22:23:51 +0200

    port changed

--- app.go
+++ app.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main(){
-    fileServer := http.FileServer(http.Dir("./static"))
+    //fileServer := http.FileServer(http.Dir("./static"))
     fmt.Println("Serving on: 80")
-    http.Handle("/", fileServer)
+    //http.Handle("/", fileServer)
+    http.HandleFunc("/", page)
     log.Fatal(http.ListenAndServe(":80", nil))
 }
 
-
+func page(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "<html><body><h1>Hello!</h1></body></html>")
+}
